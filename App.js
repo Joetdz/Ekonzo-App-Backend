6aa2b2e8415511e2b3f054f0eb05268353d13dac
Joel tondozi <joeltondozi@gmail.com>
2023-01-26 18:26:42 +0100

    feat: add racine route

--- App.js
+++ App.js
@@ -1,21 +1,23 @@
-const express = require("express");
-const cors = require("cors");
-const app = express();
-const dotenv = require("dotenv");
-const mongoose = require("mongoose");
-const userRouter = require("./Routes/user.Route");
-dotenv.config();
-app.use(cors());
+const express = require('express')
+const cors = require('cors')
+const app = express()
+const dotenv = require('dotenv')
+const mongoose = require('mongoose')
+const userRouter = require('./Routes/user.Route')
+dotenv.config()
+app.use(cors())
 
-const port = process.env.PORT;
+const port = process.env.PORT
 mongoose
   .connect(process.env.MONGO_URL, {})
-  .then(() => console.log("connecter à Mongo Db"))
-  .catch((error) => console.log(error));
+  .then(() => console.log('connecter à Mongo Db'))
+  .catch((error) => console.log(error))
 
-app.use(express.json());
-app.use("/user", userRouter);
+app.get('/', (req, res) => res.send('Hello from the backend'))
+
+app.use(express.json())
+app.use('/user', userRouter)
 
 app.listen(port, () => {
-  console.log("Server is running on port ", port);
-});
+  console.log('Server is running on port ', port)
+})
